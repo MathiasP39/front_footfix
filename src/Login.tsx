cd505b2bc907abae4371565250817510cdece1bc
mathias <mathias.puyfages@etu.umontpellier.fr>
2024-04-28 21:32:33 +0200

    Initialisation du projet par création d'un page de Login succinte

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,16 +1,49 @@
+import { Path, useForm, UseFormRegister, SubmitHandler } from "react-hook-form"
+
+interface IFormValues {
+  Email: string
+  Password: string
+}
+
+type InputProps = {
+  label: Path<IFormValues>
+  register: UseFormRegister<IFormValues>
+  type:string
+  required: boolean
+}
+
+const Input = ({ label, register, required, type }: InputProps) => (
+  <>
+    <label>{label}</label>
+    <br />
+    <input type ={type} {...register(label, { required })} />
+  </>
+)
 
 function Login() {
+
+  function handleForgotPassword (e:React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault()
+    console.log(e.currentTarget)
+    //Do something (redirect to another page)
+  }
+
+  const { register, handleSubmit } = useForm<IFormValues>()
+
+  const onSubmit: SubmitHandler<IFormValues> = (data) => {
+    alert(JSON.stringify(data))
+  }
 
   return (
     <>
       <div>
-        <form>
-        <p>Login </p>
-        <input name="email" maxLength={100} pattern="email" placeholder="email"></input>
-        <p>Password</p>
-        <input name ="password" type="password" maxLength={50} placeholder="password"></input>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <Input label="Email" register={register} required type="email"/>
+        <br />
+        <Input label="Password" register={register} required type="password"/>
         <br></br>
         <button type="submit">Se connecter</button>
+        <a href = "" onClick={handleForgotPassword}>Mot de passe oublié</a>
         </form>
       </div>
     </>
